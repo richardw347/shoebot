420563a55f65a81efff322e75cb079b5a308a5f0
Richard Williams <richardw347@gmail.com>
2022-06-14 15:43:52 +0100

    added urdf to bringup

--- shoebot_bringup/launch/bringup.launch.py
+++ shoebot_bringup/launch/bringup.launch.py
@@ -1,6 +1,6 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, PythonExpression
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command, PythonExpression
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
@@ -8,6 +8,11 @@ from launch.conditions import IfCondition
 
 
 def generate_launch_description():
+    
+    urdf_path = PathJoinSubstitution(
+        [FindPackageShare("shoebot_description"), "urdf", "shoebot.urdf.xacro"]
+    )
+        
     joy_launch_path = PathJoinSubstitution(
         [FindPackageShare('shoebot_bringup'), 'launch', 'joy_teleop.launch.py']
     )
@@ -41,4 +46,16 @@ def generate_launch_description():
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(realsense_launch_path),
         ),
+        
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[
+                {
+                    'robot_description': Command(['xacro ', urdf_path])
+                }
+            ]
+        )
     ])
